80febd0b8a373bd0ad5d7446a4948ff766e2b2de
Peng Tao <bergwolf@hyper.sh>
2026-01-07 06:06:59 +0000

    devcontainer: add initial devcontainer setup with Rust, Go, Docker, and snapshotter
    This setup facilitates a consistent and ready-to-use development container environment.
    
    Signed-off-by: Peng Tao <bergwolf@hyper.sh>

---
+++ misc/devcontainer/entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Copyright (c) 2026. Nydus Developers. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+set -eEuo pipefail
+
+function start_dockerd {
+    echo "starting dockerd"
+    # do not ever start again
+    [ -e /var/run/docker.sock ] && return 0
+    dockerd > /var/log/dockerd.log 2>&1 &
+    # try to prepare testing images
+    docker run -d -it --rm -p $REGISTRY_PORT:5000 registry:2 2>&1 &
+    docker pull ubuntu:latest 2>&1 &
+    docker pull redis:7.0.1 2>&1 &
+    docker pull nginx:latest 2>&1 &
+}
+
+function start_nydus_snapshotter {
+    echo "starting nydus snapshotter"
+    [ -e /run/containerd-nydus/containerd-nydus-grpc.sock ] && return 0
+    /usr/local/bin/containerd-nydus-grpc --config /etc/nydus/snapshotter_config.toml > /var/log/nydus-snapshotter.log 2>&1 &
+}
+
+function start_containerd {
+    echo "starting containerd"
+    [ -e /run/containerd/containerd.sock ] && return 0
+    /usr/bin/containerd --config /etc/containerd/config.toml > /var/log/containerd.log 2>&1 &
+}
+
+start_dockerd
+start_nydus_snapshotter
+start_containerd
+
+exec "$@"
